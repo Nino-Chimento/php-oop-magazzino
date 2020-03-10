5d00cee3b35ed35779b600ba3fc5a54f0aa85843
Nino Chimento <saninoc84@gmail.com>
2020-03-10 14:56:58 +0100

    funzione calco temperatura forno

--- Forno.php
+++ Forno.php
@@ -15,7 +15,13 @@
       $this->prezzo = $_prezzo;
       $this->classeEnergetica =$_classeEnergetica;
     }
+    public function calcTemperatura($_time,$_gradi)
+    {
+      return $this->temperaturaMassima = $_gradi / $_time;
+    }
   }
   $microonde = new Forno("roventa","mios",50,"A+");
+  var_dump($microonde);
+  $microonde->calcTemperatura(2,200);
   var_dump($microonde);
  ?>
