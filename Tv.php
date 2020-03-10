86637625e7d81ea08a203c0a0d3f6bac7efd3f11
Nino Chimento <saninoc84@gmail.com>
2020-03-10 13:46:02 +0100

    errore collegamento oggetto

---
+++ Tv.php
@@ -0,0 +1,14 @@
+<?php
+  include "Prodotto.php";
+  /**
+   *
+   */
+  class Tv extends ProdottoTech
+  {
+    public $schermo;
+    public $risoluzione;
+
+  }
+  $Televisione = new Tv("philips","nino")
+var_dump($ProdottoTech);
+ ?>
